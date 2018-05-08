3a890dcfb460f62ad0a7bc3767dde938b2c7d4f2
safoura.boyot@gmail.com <safoura.boyot@gmail.com>
2018-05-08 14:27:52 +0100

    write test for flipping player

---
+++ my-app/src/tictactoe.test.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
+
+import TicTacToe, {Square} from './TicTacToe'
+
+describe('tictactoe', () => {
+    it('', () => {
+        const wrapper = shallow(<TicTacToe/>)
+        const squares = wrapper.find(Square)
+        expect(squares).toHaveLength(9)
+    })
+})
+
+describe('tictactoe', () => {
+    it('should change square value on click', () => {
+        const wrapper = mount(<TicTacToe/>)
+        const square = wrapper.find(Square).at(0)
+        square.find('button').simulate('click')
+        wrapper.update()
+        expect(square.text()).toBe('X')
+    })
+})
+
+
+
+describe('tictactoe', () => {
+    it('should flipp square value on click', () => {
+        const wrapper = mount(<TicTacToe/>)
+        const firstSquare = wrapper.find(Square).at(0)
+        firstSquare.find('button').simulate('click')
+        wrapper.update()
+
+        const secondSquare = wrapper.find(Square).at(1)
+        secondSquare.find('button').simulate('click')
+        wrapper.update()
+
+        expect(secondSquare.text()).toBe('O')
+    })
+})
+
+describe('tictactoe', () => {
+    it('should announce winner when three symbol in row', () => {
+        const wrapper = mount(<TicTacToe/>)
+        const firstSquare = wrapper.find(Square).at(0)
+        firstSquare.find('button').simulate('click')
+        wrapper.update()
+
+        const secondSquare = wrapper.find(Square).at(1)
+        secondSquare.find('button').simulate('click')
+        wrapper.update()
+
+        expect(secondSquare.text()).toBe('O')
+    })
+})
